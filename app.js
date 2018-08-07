b4dc206250a15d192a93d48111fa13708786b900
ugrcngz35 <ugurcengiztr35@gmail.com>
2018-08-08 02:58:00 +0300

    VueRunner First Release
    Just Running animation with gifs and javascript actions. Vue based..

---
+++ app.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#app',
+    data: {
+        run: false,
+        left:0,
+        ended: false
+    },
+    methods: {
+        startRunning: function(){
+            const self = this;
+            setTimeout(function(){
+                self.run = !self.run;
+                self.move();
+            }, 1100);
+        },
+
+        move: function(){
+            const self = this;
+            var runInterval = setInterval(function(){
+                self.left += 3;
+                if(self.left >= 602){
+                    clearInterval(runInterval);
+                    self.run = !self.run;
+                    self.left = 722;
+                    self.ended = true;
+                }
+            }, 1);
+        },
+        playSound: function(file){
+            if(file){
+                var audio = new Audio(file);
+                audio.play();
+            }
+        }
+    },
+    computed: {
+        isRunning: function(){
+            return {
+                isrunning: this.run
+            }
+        }
+    }
+})
